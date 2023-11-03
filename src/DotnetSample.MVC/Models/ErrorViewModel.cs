d33b1f740a5349165d7dac477455f7d14a79ebfc
madeth <madethg@gmail.com>
2023-11-03 18:56:24 +0900

    dotnet new mvc -o src/DotnetSample.MVC

---
+++ src/DotnetSample.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetSample.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
